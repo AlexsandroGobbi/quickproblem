e5d58747778c5a560de66d413ef1cb11ea15cd02
Gobbi <aalex.gobbi@hotmail.com>
2022-11-03 20:16:34 -0300

    Projeto

---
+++ Modulo_II/src/gobbi/alexsandro/aula2405/Pessoa.java
@@ -0,0 +1,11 @@
+package gobbi.alexsandro.aula2405;
+
+public class Pessoa {
+    private String nome = "Juliana";
+    private String sobrenome = "Dal Olio";
+    private String apelido = "Ju";
+
+    private int aniversarioDia;
+    private int aniversarioMes;
+
+}
