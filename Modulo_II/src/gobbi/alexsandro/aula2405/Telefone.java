a7251d554e55295132cedcdf85091f4502e45642
AlexsandroGobbi <105758441+AlexsandroGobbi@users.noreply.github.com>
2022-11-23 19:46:13 -0300

    Delete Modulo_II directory

--- Modulo_II/src/gobbi/alexsandro/aula2405/Telefone.java
+++
@@ -1,9 +0,0 @@
-package gobbi.alexsandro.aula2405;
-
-public class Telefone {
-    private String ddd = "33";
-    private String numero = "2222-5555";
-    private String telefone = "fixo";
-    private String celular = "(33) 9999-5555";
-    private String operadora = "vivo";
-}
